43ba0258bcc7b61621008c9675e3469e3eac507e
nahuel ferrino <nahuelif08@gmail.com>
2026-09-07 18:42:03 -0300

    Elimino Archivo .db por pedido

--- Biblioteca/AccesoDatos/obj/Debug/net10.0/AccesoDatos.AssemblyInfo.cs
+++ Biblioteca/AccesoDatos/obj/Debug/net10.0/AccesoDatos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08aaa23b0a372bac80fd07fdef6a909cb2a0aa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed6969cc3f71b4154b4b6a6439c3f562b69ed02")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
